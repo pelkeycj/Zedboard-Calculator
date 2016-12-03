ba4175685947018346bfaa0888d285a741be1fd9
Connor Pelkey <Pelkey.c@husky.neu.edu>
2016-12-03 16:15:20 -0500

    allows continuous performance of operations

--- Calculator.cpp
+++ Calculator.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
-//#include <stdlib.h>
 #include <fcntl.h>
 #include <unistd.h>
 #include <sys/mman.h>
 #include <cmath>
 #include "Zedboard.h"
 using namespace std;
+
+void Run(Zedboard z, int states[]);
 
 int main()
 {
@@ -19,9 +20,9 @@ int main()
 	cout << "- right for subtraction\n";
 	cout << "- up for multiplicaton\n";
 	cout << "- down for division\n";
-	cout << " -center for result\n\n";
+	cout << "- center for result\n\n";
 
-	char response = 'y'
+	char response = 'y';
 	while (response == 'y')
 	{
 		// Initial state of pushbuttons set to 0
@@ -35,131 +36,75 @@ int main()
 		z.SetLedNumber(0);
 
 		// Run application once
-		Run(z);
+		Run(z, states);
 
-/*	for (int i = 0; i < 5; i++)
-	{
-		states[i] = z.RegisterRead(gpio_pbtnl_offset + 4*i);
-	}
-
-	for (int i = 0; i < 8; i++)
-	{
-		led_value += z.RegisterRead(gpio_led1_offset + 4*i) * pow(2,i);
-	}
-
-		int value = 0;
-		int reset_value = 0;
-
-		bool buttonPressed = false;
-		int num1 = 0;
-		int num2 = 0;
-		int total = 0;
-		int nums = 0;
-		int final = 0;
-
-		while (true)
-		{
-			nums = 0;
-			buttonPressed = false;
-			// Read user input
-			while(nums < 2)
-			{
-				// read switches and display using LEDs
-				for (int i = 0; i <= 7; i++)
-				{
-					value = z.RegisterRead(gpio_sw1_offset + 4*i);
-					z.RegisterWrite(gpio_led1_offset + 4*i, value);
-				}
-				//
-				if (z.PushButtonGet(states) == 5) // Button Center Pushed
-				{
-					nums++;
-					led_value = 0;
-
-					led_value += z.RegisterRead(gpio_sw1_offset) * pow(2,0);
-					cout << "sw1 status: " << z.RegisterRead(gpio_sw1_offset) << endl;
-					led_value += z.RegisterRead(gpio_sw2_offset) * pow(2,1);
-					led_value += z.RegisterRead(gpio_sw3_offset) * pow(2,2);
-					led_value += z.RegisterRead(gpio_sw4_offset) * pow(2,3);
-					led_value += z.RegisterRead(gpio_sw5_offset) * pow(2,4);
-					led_value += z.RegisterRead(gpio_sw6_offset) * pow(2,5);
-					led_value += z.RegisterRead(gpio_sw7_offset) * pow(2,6);
-					led_value += z.RegisterRead(gpio_sw8_offset) * pow(2,7);
-					if (nums == 1)
-					{
-						num1 = led_value;
-						cout << "Center Button Pressed: Num" << nums << " = " << num1 << endl;
-					}
-					else
-					{
-						num2 = led_value;
-						cout << "Center Button Pressed: Num" << nums << " = " << num2 << endl;
-					}
-				}
-			}
-			cout << "Now Both Numbers Have Been Entered" << endl;
-			final = 0;
-			while (!buttonPressed)
-			{
-				for (int i = 0; i < 5; i++)
-				{
-					value = z.PushButtonGet(states);
-					switch(value)
-					{
-						case 1:	// Button Left pushed
-							final = num1 + num2;
-							cout << num1 << " + " << num2 << " = " << final << endl;
-							z.SetLedNumber(final);
-							buttonPressed = true;
-							break;
-						case 2: // Button Right pushed
-							final = num1 - num2;
-							cout << num1 << " - " << num2 << " = " << final << endl;
-							z.SetLedNumber(final);
-							buttonPressed = true;
-							break;
-						case 3: // Button Up pushed
-							final = num1 * num2;
-							cout << num1 << " * " << num2 << " = " << final << endl;
-							z.SetLedNumber(final);
-							buttonPressed = true;
-							break;
-						case 4: // Button Down pushed
-							final = num1 / num2;
-							cout << num1 << " / " << num2 << " = " << final << endl;
-							z.SetLedNumber(final);
-							buttonPressed = true;
-							break;
-						case 5: // Button Center Pushed
-							cout << "Please press one of the other 4 buttons" << endl;
-							break;
-					}
-				}
-			}
-		}
-		*/
 		cout << "\nRun again? y/n: ";
-		cin >> response
+		cin >> response;
 	}
 }
 
-void Run(Zedboard z)
+void Run(Zedboard z, int states[])
 {
 	bool centerPushed = false;
 	int total = 0;
-	int number_count = 0;
+	bool first_num = true;
 	int switch_value = 0;
+	int button = 0;
 
-	// while !centerPushed :::
-	// read button states
-	// if button pressed != center , read number from switches
-	//	if first number being entered
-	//		if multiplication or division
-	//    	set total to 1 and perform operation using switch_value
-	// 		else add/subtract switch_value from total
-	//	else perform operation on total using switch_value
-	// add 1 to number count, reset switch_value to 0
-	// if center pushed
-	// display total on leds,
-	// return
+	while (!centerPushed)
+	{
+		// get pressed button
+		button = z.PushButtonGet(states);
+		if (button != 0)
+		{
+			// read switches
+			switch_value += z.RegisterRead(gpio_sw1_offset) * pow(2,0);
+			switch_value += z.RegisterRead(gpio_sw2_offset) * pow(2,1);
+			switch_value += z.RegisterRead(gpio_sw3_offset) * pow(2,2);
+			switch_value += z.RegisterRead(gpio_sw4_offset) * pow(2,3);
+			switch_value += z.RegisterRead(gpio_sw5_offset) * pow(2,4);
+			switch_value += z.RegisterRead(gpio_sw6_offset) * pow(2,5);
+			switch_value += z.RegisterRead(gpio_sw7_offset) * pow(2,6);
+			switch_value += z.RegisterRead(gpio_sw8_offset) * pow(2,7);
+
+			if (first_num) //handle first number being entered
+			{
+				total += switch_value;
+				first_num = false;
+				cout << total << endl;
+			}
+			else
+			{
+				//perform operation
+				switch(button)
+				{
+					case 1: //left button -> addition
+						cout << "+ " << switch_value << endl;
+						total += switch_value;
+						switch_value = 0;
+						break;
+					case 2: //right button -> subtraction
+						cout << "- " << switch_value << endl;
+						total -= switch_value;
+						switch_value = 0;
+						break;
+					case 3: //up button -> multiplication
+						cout << "* " << switch_value << endl;
+						total *= switch_value;
+						switch_value = 0;
+						break;
+					case 4: //down button -> division
+						cout << "/ " << switch_value << endl;
+						total /= switch_value;
+						switch_value = 0;
+						break;
+					case 5: // center button -> equals
+						cout << "= " << total << endl << endl;
+						centerPushed = true;
+						z.SetLedNumber(total);
+						break;
+				}
+			}
+		}
+	}
 }
